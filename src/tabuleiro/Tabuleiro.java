28e184a60ec5809df42a39393896eda780d2a08e
zadigando <zadigdebem@gmail.com>
2021-08-25 13:44:36 -0300

    Placing pieces on the Board

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -1,16 +1,16 @@
 package tabuleiro;
 
 public class Tabuleiro {
-	
+
 	private int linhas;
 	private int colunas;
 	private Peca[][] pecas;
-	
+
 	public Tabuleiro(int linha, int coluna) {
 		this.linhas = linha;
 		this.colunas = coluna;
 		pecas = new Peca[linhas][colunas];
-		
+
 	}
 
 	public int getLinhas() {
@@ -28,17 +28,19 @@ public class Tabuleiro {
 	public void setColunas(int colunas) {
 		this.colunas = colunas;
 	}
-	
+
 	public Peca peca(int linha, int coluna) {
 		return pecas[linha][coluna];
 	}
-	
+
 	public Peca peca(Posicao posicao) {
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
-	
-	
-	
+
+	public void botarPeca(Peca peca, Posicao posicao) {
+		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
+		peca.posicao = posicao;
+	}
 	
 	
 
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,6 +1,9 @@
 package xadrez;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
 
@@ -8,6 +11,7 @@ public class PartidaXadrez {
 
 	public PartidaXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		comecoDoJogo();
 	}
 
 	public PecaDeXadrez[][] getPecas() {
@@ -20,5 +24,13 @@ public class PartidaXadrez {
 		}
 		return mat;
 	}
+
+	private void comecoDoJogo() {
+		tabuleiro.botarPeca(new Torre(tabuleiro, Cores.BRANCO), new Posicao(2, 1));
+		tabuleiro.botarPeca(new Rei(tabuleiro, Cores.PRETO), new Posicao(0, 4));
+		tabuleiro.botarPeca(new Rei(tabuleiro, Cores.BRANCO), new Posicao(7, 4));
+
+	}
+	
 
 }
