3dd4fbc438671446e559b30e905a6906cd668f32
zadigando <zadigdebem@gmail.com>
2021-08-25 11:26:32 -0300

    First Class: Position

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import tabuleiro.Posicoes;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Posicoes pos = new Posicoes(3, 5);
+		System.out.println(pos);
+	}
+
+}
